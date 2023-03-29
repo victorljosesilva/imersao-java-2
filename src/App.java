34b4638b983b4b9dcf606bd2d6f1b8fd0f5927ad
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:49:49 -0300

    feature: criando listas de filmes do Json

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 	public static void main(String[] args) throws Exception {
@@ -13,5 +15,6 @@ public class App {
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		
 		JsonParser parser = new JsonParser();
+		List<Map<String, String>> listaDeFilmes = parser.parse(response.body());
 	}
 }
