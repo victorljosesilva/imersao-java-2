a65f3bd2aac15751d32cb618ebc9876366463060
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:58:07 -0300

    action: dando um espaço entre cada item-filme

--- src/App.java
+++ src/App.java
@@ -20,6 +20,7 @@ public class App {
 			System.out.println(filme.get("title"));
 			System.out.println(filme.get("image"));
 			System.out.println(filme.get("imDbRating"));
+			System.out.println();
 		}
 	}
 }
