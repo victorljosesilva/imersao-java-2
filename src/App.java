83a6e6b0be2714d444f6f9ec880fd49d0ef4bec1
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:37:33 -0300

    fazendo uma requesição HTTP

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
 	public static void main(String[] args) {
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		HttpClient client = HttpClient.newHttpClient();
 		URI endereco = URI.create(url);
+		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
 	}
 }
