291e8133c31e7c8fbad0dc91962f1d17f58da390
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:31:08 -0300

    feature: transformando url numa URI

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class App {
 	public static void main(String[] args) {
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		HttpClient client = HttpClient.newHttpClient();
+		URI endereco = URI.create(url);
 	}
 }
