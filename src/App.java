a85b28ae42ffec3cb2173cff15a194a54cd5d918
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:25:25 -0300

    feature: variável url com o endereço para requesição

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
+import java.net.URI;
 
 public class App {
 	public static void main(String[] args) {
-		
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 	}
 }
