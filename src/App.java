c24c48b4df0e9a11e58e2ad546c65b2e20875528
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:26:46 -0300

    feature: criando um HttpClient

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 import java.net.URI;
+import java.net.http.HttpClient;
 
 public class App {
 	public static void main(String[] args) {
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+		HttpClient client = HttpClient.newHttpClient();
 	}
 }
