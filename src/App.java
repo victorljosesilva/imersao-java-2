51b746f7ba4062d4b73e228892f1ebafda20ba5d
Victor Leonardo José da Silva <e054820@gmail.com>
2023-03-29 10:41:52 -0300

    feature: guardando Json na variável response

--- src/App.java
+++ src/App.java
@@ -1,13 +1,16 @@
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 		HttpClient client = HttpClient.newHttpClient();
 		URI endereco = URI.create(url);
 		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		
 	}
 }
